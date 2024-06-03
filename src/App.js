f8b11a2091270b5463b15ebcddd746272e4a8931
Devu <devudevu10@gmail.com>
2024-06-03 23:01:06 +1000

    use state

--- src/App.js
+++ src/App.js
@@ -4,16 +4,26 @@ import ProfileCom from "./UserComponent";
 import { UserBio } from "./UserComponent";
 
 const App = () => {
-  const [name] = useState("devendra dahal");
+  // const [name] = useState("devendra dahal");
 
-  const bio =
-    "lorElit amet officia minim dolor ad deserunt nisi sit voluptate ullamco.";
+  // const bio =
+  //   "lorElit amet officia minim dolor ad deserunt nisi sit voluptate ullamco.";
+  const [counter, setCounter] = useState(10);
+
+  const increase = () => {
+    setCounter(counter + 1);
+  };
+  console.log(counter);
   return (
     <>
-      <ProfileCom snameProps={name} spropsBio={bio} />
+      hello
+      {/* <ProfileCom snameProps={name} spropsBio={bio} />
       <ProfileCom snameProps={"swek ss"} spropsBio="alpha" />
       <ProfileCom snameProps="anav dahal" spropsBio="beta" />
-      <UserBio />
+      <UserBio /> */}
+      <div>{counter}</div>
+      <button onClick={increase}>+</button>
+      <button>-</button>
     </>
   );
 };
