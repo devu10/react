aac2c852935097eae2f2c034ea5db8c40616b913
Devu <devudevu10@gmail.com>
2024-06-06 22:50:23 +1000

    uncontrolled component inreact

--- src/MyForm.js
+++ src/MyForm.js
@@ -1,12 +1,34 @@
+import { useRef } from "react";
 export const MyForm = () => {
+  const addressRef = useRef("");
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    // console.log(e);
+    console.log(addressRef.current.value);
+  };
+
+  const changeForm = (e) => {
+    const str = e.target.value;
+    console.log(str);
+  };
+  console.log(addressRef);
   return (
-    <form action="" className="card">
+    <form action="" className="card" onSubmit={submitForm}>
       <div className="mb-3">
+        <input
+          type="text"
+          className="form-control mb-3"
+          required
+          placeholder="name"
+          onChange={changeForm}
+        ></input>
         <input
           type="text"
           className="form-control"
           required
-          placeholder="name"
+          placeholder="email"
+          ref={addressRef}
         ></input>
       </div>
       <button type="submit">Submit</button>
